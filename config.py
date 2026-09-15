529afefda8ffdaadf081176202b5cabcadd18c28
diseekhati-dotcom <diseekhati@gmail.com>
2026-09-15 08:38:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,45 @@
+#@cantarellabots
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.environ.get("API_ID", ))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+SET_INTERVAL = int(os.environ.get("SET_INTERVAL", 60))  # in seconds, default 1 hour
+TARGET_CHAT_ID = os.environ.get("TARGET_CHAT_ID", "")
+MAIN_CHANNEL = os.environ.get("MAIN_CHANNEL", "") # Change as needed
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_NAME = os.environ.get("MONGO_NAME", "cantarellabots")
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+ADMIN_URL = os.environ.get("ADMIN_URL", "@V_Sbotmaker")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+FSUB_PIC = os.environ.get("FSUB_PIC", "https://files.catbox.moe/bli70r.jpg")
+FSUB_LINK_EXPIRY = int(os.environ.get("FSUB_LINK_EXPIRY", 600))
+START_PIC =os.environ.get("START_PIC", "https://files.catbox.moe/4b8jvw.jpg")
+
+# ─── Filename & Caption Formats ───
+FORMAT = os.environ.get("FORMAT", "[S{season}-E{episode}] {title} [{quality}] [{audio}]")
+CAPTION = os.environ.get("CAPTION", "[ @cantarellabots {FORMAT}]")
+
+# ─── Progress Bar Settings ───
+PROGRESS_BAR = os.environ.get("PROGRESS_BAR", """
+<blockquote> {bar} </blockquote>
+<blockquote>📁 <b>{title}</b>
+⚡ Speed: {speed}
+📦 {current} / {total}</blockquote>
+""")
+
+# ─── Response Images ───
+# Rotating anime images sent with every bot reply. Add as many as you like.
+RESPONSE_IMAGES = [
+    "https://files.catbox.moe/5oonsm.jpg",
+    "https://files.catbox.moe/9ufgme.jpg",
+    "https://files.catbox.moe/4b8jvw.jpg",
+    "https://files.catbox.moe/bli70r.jpg",
+    "https://files.catbox.moe/uce0lw.jpg",
+    "https://files.catbox.moe/is7q4q.jpg"
+]
